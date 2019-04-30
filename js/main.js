a6c03ea248a58ff55e469b11834ad2983265b984
ruizdurazo <enrique.ruizdurazo@gmail.com>
2019-04-29 20:42:40 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-/*
+/* 
  * Enrique Ruiz Durazo
  * 2019
  */
 
-var i = 0
+// Get number of words, location to render 
 var word_count = words.length
 var word_element = $('#slideshow-anim')
 
+// Animation durations
 var anim_delay = 4000 //ms
 var anim_duration = 500 //ms
 
-var timerId
+// Counter
 var num = 0
 
+// Function
 $(document).ready(function() {
   setInterval(function() {
     num++
