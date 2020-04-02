97286d74bebdc14107172b853d458262bad55170
ruizdurazo <enrique.ruizdurazo@gmail.com>
2020-04-03 00:13:59 +0200

    Redesign changes

---
+++ js/blog-posts.js
@@ -0,0 +1,11 @@
+// Blog posts appear in order specified.
+const posts = [
+  {
+    folder: 'hello-universe',
+    thumbnail: false
+  }
+  // {
+  //   folder:'blog-post-example',
+  //   thumbnail: false
+  // }
+]
--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,12 @@
+/*
+ * Enrique Ruiz Durazo
+ * 2020
+ */
+
 let menuSVG = document.getElementById('menu-svg')
-document.getElementById('menu').addEventListener('click', event => {
+document.getElementById('menu-button').addEventListener('click', event => {
   if (menuSVG.alt === 'Menu') {
+    document.getElementById('menu-button').blur()
     document.getElementById('menu-overlay').setAttribute('style', 'max-height: 100vh;')
     document.getElementById('menu-overlay').innerHTML =
       '<div class="lang-list">' +
@@ -11,9 +17,22 @@ document.getElementById('menu').addEventListener('click', event => {
     menuSVG.src = '/assets/images/icon-close.svg'
     menuSVG.alt = 'Close'
   } else {
+    document.getElementById('menu-button').blur()
     document.getElementById('menu-overlay').setAttribute('style', 'max-height: 0;')
     document.getElementById('menu-overlay').innerHTML = ''
     menuSVG.src = '/assets/images/icon-menu.svg'
     menuSVG.alt = 'Menu'
   }
 })
+
+document.addEventListener(
+  'keydown',
+  event => {
+    if (event.keyCode === 27) {
+      if (document.getElementById('menu-svg').alt === 'Close') {
+        document.getElementById('menu-button').click()
+      }
+    }
+  },
+  true
+)
