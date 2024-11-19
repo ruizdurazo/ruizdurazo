e697cfec031f38ce814ccaf6e8ac247eb1da542c
Enrique Ruiz Durazo <enrique.ruizdurazo@gmail.com>
2024-11-19 15:17:00 +0100

    Update navbar

--- js/navbar.js
+++ js/navbar.js
@@ -3,49 +3,51 @@
  * 2021-2025
  */
 
-const navbar = document.getElementById('navbar')
-const menuSVG = document.getElementById('menu-svg')
-document.getElementById('menu-button').addEventListener('click', (event) => {
-  if (menuSVG.alt === 'Menu') {
-    document.getElementById('menu-button').blur()
+const navbar = document.getElementById("navbar");
+const menuSVG = document.getElementById("menu-svg");
+document.getElementById("menu-button").addEventListener("click", (event) => {
+  if (menuSVG.alt === "Menu") {
+    document.getElementById("menu-button").blur();
+    navbar.classList.remove("menu-close");
+    navbar.classList.add("menu-open");
     document
-      .getElementById('menu-overlay')
-      .setAttribute('style', 'max-height: 100svh;')
-    document.getElementById('menu-overlay').innerHTML =
+      .getElementById("menu-overlay")
+      .setAttribute("style", "max-height: 100svh;");
+    document.getElementById("menu-overlay").innerHTML =
       '<div class="menu-list">' +
       '<a class="menu-list-item" href="/">Home</a>' +
       '<a class="menu-list-item" href="mailto:enrique@ruizdurazo.com" target="_blank">Email</a>' +
       '<a class="menu-list-item" href="https://x.com/ruizdurazo" target="_blank">X (Twitter)</a>' +
       '<a class="menu-list-item" href="https://github.com/ruizdurazo" target="_blank">GitHub</a>' +
       '<a class="menu-list-item" href="/lol">LinkedIn</a>' +
-      '</div>'
-    menuSVG.src = '/assets/images/icon-close.svg'
-    menuSVG.alt = 'Close'
-    menuSVG.width = '60'
-    menuSVG.height = '60'
-    navbar.style.backgroundColor = '#fff'
+      "</div>";
+    menuSVG.src = "/assets/images/icon-close.svg";
+    menuSVG.alt = "Close";
+    menuSVG.width = "60";
+    menuSVG.height = "60";
   } else {
-    document.getElementById('menu-button').blur()
+    document.getElementById("menu-button").blur();
+    navbar.classList.remove("menu-open");
+    navbar.classList.add("menu-close");
     document
-      .getElementById('menu-overlay')
-      .setAttribute('style', 'max-height: 0;')
-    document.getElementById('menu-overlay').innerHTML = ''
-    menuSVG.src = '/assets/images/icon-menu.svg'
-    menuSVG.alt = 'Menu'
-    menuSVG.width = '60'
-    menuSVG.height = '60'
-    navbar.style.backgroundColor = '#ffffffdd'
+      .getElementById("menu-overlay")
+      .setAttribute("style", "max-height: 0;");
+    document.getElementById("menu-overlay").innerHTML = "";
+    menuSVG.src = "/assets/images/icon-menu.svg";
+    menuSVG.alt = "Menu";
+    menuSVG.width = "60";
+    menuSVG.height = "60";
   }
-})
+});
 
 document.addEventListener(
-  'keydown',
+  "keydown",
   (event) => {
-    if (event.key === 'Escape') {
-      if (document.getElementById('menu-svg').alt === 'Close') {
-        document.getElementById('menu-button').click()
+    if (event.key === "Escape") {
+      if (document.getElementById("menu-svg").alt === "Close") {
+        document.getElementById("menu-button").click();
       }
     }
   },
   true
-)
+);
--- js/notes.js
+++ js/notes.js
@@ -1,22 +1,22 @@
 // Notes appear according to date specified.
 const notes = [
   {
-    folder: 'on-javascript-typescript',
+    folder: "on-javascript-typescript",
     thumbnail: false,
-    thumbnailImage: '',
-    thumbnailImageAlt: '',
-    date: '2022-07-07',
-    title: 'On JavaScript / TypeScript',
-    description: 'Snippets and tips on JavaScript and TypeScript.',
+    thumbnailImage: "",
+    thumbnailImageAlt: "",
+    date: "2022-07-07",
+    title: "On JavaScript / TypeScript",
+    description: "Snippets and tips on JavaScript and TypeScript.",
   },
   {
-    folder: 'on-css-styling',
+    folder: "on-css-styling",
     thumbnail: false,
-    thumbnailImage: '',
-    thumbnailImageAlt: '',
-    date: '2021-11-14',
-    title: 'On CSS / Styling',
-    description: 'Snippets and tips on CSS, SCSS/Sass, and styling in general.',
+    thumbnailImage: "",
+    thumbnailImageAlt: "",
+    date: "2021-11-14",
+    title: "On CSS / Styling",
+    description: "Snippets and tips on CSS, SCSS/Sass, and styling in general.",
   },
   // {
   //   folder: 'on-markdown',
@@ -28,13 +28,13 @@ const notes = [
   //   description: 'A complete guide on Markdown syntax.',
   // },
   {
-    folder: 'hello-universe',
+    folder: "hello-universe",
     thumbnail: false,
-    thumbnailImage: '',
-    thumbnailImageAlt: '',
-    date: '2020-04-02',
-    title: 'Hello, Universe!',
-    description: 'An origin story.',
+    thumbnailImage: "",
+    thumbnailImageAlt: "",
+    date: "2020-04-02",
+    title: "Hello, Universe!",
+    description: "An origin story.",
   },
   // {
   //   folder:'note-example',
@@ -45,4 +45,4 @@ const notes = [
   //   title: 'Note Title',
   //   description: 'Description of the note.',
   // }
-]
+];
